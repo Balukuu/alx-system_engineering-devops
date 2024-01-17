c3dc480696994e599b77821f7d1eca15553a4d21
Edgar Baluku <edgarbaluku@gmail.com>
2024-01-17 09:29:03 +0300

    2r

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,48 +1,31 @@
+#!/usr/bin/python3
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
-def recurse(subreddit, hot_list=None, after=None):
-    if hot_list is None:
-        hot_list = []
 
-    if after is None:
-        url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    else:
-        url = f'https://www.reddit.com/r/{subreddit}/hot.json?after={after}'
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
-        for post in posts:
-            title = post.get('data', {}).get('title', '')
-            hot_list.append(title)
-
-        after = data.get('data', {}).get('after', None)
-        if after:
-            return recurse(subreddit, hot_list, after)
-        else:
-            return hot_list
-    except requests.exceptions.HTTPError as e:
-        if response.status_code == 404:
-            print(f"Error: Subreddit '{subreddit}' not found.")
-            return None
-        else:
-            print(f"Error: {e}")
-            return None
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        result = recurse(subreddit)
-        if result is not None:
-            print(len(result))
-        else:
-            print("None")
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
 
